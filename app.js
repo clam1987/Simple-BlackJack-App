71865f869a8418d39c120f87e6c7d772b1046023
Cuong Lam <lin-1987@live.com>
2020-02-18 14:05:12 -0800

    Initial Files

---
+++ app.js
@@ -0,0 +1,17 @@
+// Deal Cards + Setup Cards
+// Create a Function to deal cards + logic for Ace
+// 1.Cards have a value of 1-11=> 2-Ace
+// 1a. Each card value has 4 instances
+// 1ab. Each value can only be picked a maximum of 4 times
+// 1abc. Put a condition that card 1 has a value of either 1 or 11.
+// Cards must be dealt out one at a time.
+// Add cards value until 21
+// Create a function for game logic
+// 2. Cards dealt to player then cpu
+// 2a. Add the total value from player + cpu
+// 2ab repeat until stop or bust
+// 2abc create a stop function
+// 2abcd stop function will pass turn and give decision to cpu 
+// If over 21 lose
+// If player value is higher then cpu value win otherwise, lose.
+// Dealer has to hit at 16
